f6a6d80beeac6bd1085a0a0e85821e2e975c426e
kangssu <gmltn0475@naver.com>
2023-11-11 21:10:44 +0900

    feat: 사용자 로그인 API 완료.

--- src/enum/errorHttpStatus.enum.ts
+++ src/enum/errorHttpStatus.enum.ts
@@ -1,3 +1,5 @@
 export enum ErrorHttpStatus {
+  NOT_FOUND = 404,
   CONFLICT = 409,
+  BAD_REQEUST = 400,
 }
--- src/feature/user/user.module.ts
+++ src/feature/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserController } from './user.controller';
 import { User } from 'src/entity/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
+import { UserLib } from './user.lib';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserLib],
+  exports: [UserLib],
 })
 export class UserModule {}
